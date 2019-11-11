14323cb5932666505110937a7e3f9742a89a48f6
Paul Solt <PaulSolt@gmail.com>
2019-11-11 14:04:46 -0500

    Added logic to parse from Objective-C to Swift dictionaries with NSJSONSerialization

--- iOS9-Star-Wars-Hybrid/iOS9-Star-Wars-Hybrid/PersonController.swift
+++ iOS9-Star-Wars-Hybrid/iOS9-Star-Wars-Hybrid/PersonController.swift
@@ -13,30 +13,51 @@ class PersonController: NSObject {
 	// https://swapi.co/api/people?search=luke
 	private let baseURL = URL(string: "https://swapi.co/api/people")!
 	
-//	override init() {
-//		let person = LSIPerson()
-//	}
+	//	override init() {
+	//		let person = LSIPerson()
+	//	}
 	
 	@objc func searchForPeople(with searchTerm: String,
-						 completion: @escaping ([LSIPerson]?, Error?) -> Void) {
+							   completion: @escaping ([LSIPerson]?, Error?) -> Void) {
 		var components = URLComponents(url: self.baseURL, resolvingAgainstBaseURL: true)!
 		
-        let searchItem = URLQueryItem(name: "search", value: searchTerm)
-        components.queryItems = [searchItem]
-        let url = components.url!
-
+		let searchItem = URLQueryItem(name: "search", value: searchTerm)
+		components.queryItems = [searchItem]
+		let url = components.url!
+		
 		// TODO: DispatchQueue in API code vs. client code
 		
-        URLSession.shared.dataTask(with: url) { (data, _, error) in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
+		URLSession.shared.dataTask(with: url) { (data, _, error) in
+			if let error = error {
+				completion(nil, error)
+				return
+			}
 			
+			guard let data = data else {
+				completion(nil, NSError()) // TODO: provide error details
+				return
+			}
 			
+			// Parse data
+			do {
+				// dictionary
+				// results
+				
+				guard let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any],
+					let personDictionaries = dictionary["results"] as? [[String : Any]]
+					else {
+						throw NSError()
+				}
+				
+				// create array of people
+				let people = personDictionaries.compactMap { LSIPerson(dictionary: $0) }
+				completion(people, nil)
+				
+			} catch {
+				completion(nil, error)
+			}
 			
-			
-        }.resume()
-	
+		}.resume()
+		
 	}
 }
