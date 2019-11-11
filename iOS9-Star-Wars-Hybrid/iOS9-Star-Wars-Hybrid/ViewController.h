632e1291c734246f5d7a5f1d5595e0f29c269546
Paul Solt <PaulSolt@gmail.com>
2019-11-11 13:14:53 -0500

    Star Wars starter project

---
+++ iOS9-Star-Wars-Hybrid/iOS9-Star-Wars-Hybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS9-Star-Wars-Hybrid
+//
+//  Created by Paul Solt on 11/11/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS9-Star-Wars-Hybrid/iOS9-Star-Wars-Hybrid/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOS9-Star-Wars-Hybrid
+//
+//  Created by Paul Solt on 11/11/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
