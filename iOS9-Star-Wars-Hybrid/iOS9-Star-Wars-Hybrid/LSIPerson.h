136c98017917bc48bf2fb08f1dc3bc29041ee15d
Paul Solt <PaulSolt@gmail.com>
2019-11-11 13:17:35 -0500

    Added model object

---
+++ iOS9-Star-Wars-Hybrid/iOS9-Star-Wars-Hybrid/LSIPerson.h
@@ -0,0 +1,26 @@
+//
+//  LSIPerson.h
+//  iOS9-Star-Wars-Hybrid
+//
+//  Created by Paul Solt on 11/11/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSIPerson : NSObject
+
+@property (nonatomic, copy, readonly) NSString *name;
+@property (nonatomic, copy, readonly) NSString *birthYear;
+@property (nonatomic, readonly) double height;
+@property (nonatomic, copy, readonly) NSString *eyeColor;
+
+- (instancetype)initWithName:(NSString *)name
+                   birthYear:(NSString *)birthYear
+                      height:(double)height
+                    eyeColor:(NSString *)eyeColor;
+
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
